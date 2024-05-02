1724d63c387e6060f1f5b0b2d8513f3d99963bd4
zhaleh <zhalehelmi@gmail.com>
2024-05-02 14:58:10 +0330

    add typewriter

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Header.css";
 import Tobbar from "../Topbar/Tobbar";
 import Navbar from "../Navbar/Navbar";
+import Landing from "../Landing/Landing";
 
 const Header = () => {
   return (
     <header className="header">
       <Tobbar />
       <Navbar />
+      <Landing />
     </header>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+
 import "./style/reset.css";
 import "./style/fonts.css";
 import "./style/variables.css";
