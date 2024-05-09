32334377f39cd09c5060bca70ec26a807ed21e4e
zhaleh <zhalehelmi@gmail.com>
2024-05-09 14:41:44 +0330

    category page

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Pagination.css";
+const Pagination = () => {
+  return (
+    <div className="courses-pagination">
+      <ul className="courses__pagination-list">
+        <li className="courses__pagination-item">
+          <a href="#" className="courses__pagination-link">
+            <i className="fas fa-long-arrow-alt-right courses__pagination-icon"></i>
+          </a>
+        </li>
+        <li className="courses__pagination-item">
+          <a href="#" className="courses__pagination-link">
+            1
+          </a>
+        </li>
+        <li className="courses__pagination-item">
+          <a href="#" className="courses__pagination-link">
+            2
+          </a>
+        </li>
+        <li className="courses__pagination-item">
+          <a
+            href="#"
+            className="courses__pagination-link courses__pagination-link--active"
+          >
+            3
+          </a>
+        </li>
+        <li className="courses__pagination-item">
+          <a href="#" className="courses__pagination-link">
+            <i className="fas fa-long-arrow-alt-left courses__pagination-icon"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,7 +1,84 @@
 import React from "react";
 import "./Category.css";
+import TopBar from "../../components/Topbar/Tobbar";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import CourseBox from "../../components/CourseBox/CourseBox";
+import Pagination from "../../components/Pagination/Pagination";
+
 const Category = () => {
-  return <div>Category Page</div>;
+  return (
+    <>
+      <TopBar />
+      <Navbar />
+
+      <section className="courses">
+        <div className="container">
+          <div className="courses-top-bar">
+            <div className="courses-top-bar__right">
+              <div className="courses-top-bar__row-btn courses-top-bar__icon--active">
+                <i className="fas fa-border-all courses-top-bar__icon"></i>
+              </div>
+              <div className="courses-top-bar__column-btn">
+                <i className="fas fa-align-left courses-top-bar__icon"></i>
+              </div>
+
+              <div className="courses-top-bar__selection">
+                <span className="courses-top-bar__selection-title">
+                  مرتب سازی پیش فرض
+                  <i className="fas fa-angle-down courses-top-bar__selection-icon"></i>
+                </span>
+                <ul className="courses-top-bar__selection-list">
+                  <li className="courses-top-bar__selection-item courses-top-bar__selection-item--active">
+                    مرتب سازی پیش فرض
+                  </li>
+                  <li className="courses-top-bar__selection-item">
+                    مربت سازی بر اساس محبوبیت
+                  </li>
+                  <li className="courses-top-bar__selection-item">
+                    مربت سازی بر اساس امتیاز
+                  </li>
+                  <li className="courses-top-bar__selection-item">
+                    مربت سازی بر اساس آخرین
+                  </li>
+                  <li className="courses-top-bar__selection-item">
+                    مربت سازی بر اساس ارزان ترین
+                  </li>
+                  <li className="courses-top-bar__selection-item">
+                    مربت سازی بر اساس گران ترین
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="courses-top-bar__left">
+              <form action="#" className="courses-top-bar__form">
+                <input
+                  type="text"
+                  className="courses-top-bar__input"
+                  placeholder="جستجوی دوره ..."
+                />
+                <i className="fas fa-search courses-top-bar__search-icon"></i>
+              </form>
+            </div>
+          </div>
+
+          <div className="courses-content">
+            <div className="container">
+              <div className="row">
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+              </div>
+            </div>
+          </div>
+          <Pagination />
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
 };
 
 export default Category;
