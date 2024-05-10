04f21692dd2f28b8c9966e582a3725152d8a7971
zhaleh <zhalehelmi@gmail.com>
2024-05-10 17:38:18 +0330

    course detail box

--- src/pages/courseinfo/CourseInfo.jsx
+++ src/pages/courseinfo/CourseInfo.jsx
@@ -4,6 +4,8 @@ import Topbar from "../../components/Topbar/Topbar";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
+import poster from "../../assets/images/courses/js_project.png";
+import CourseDetailBox from "../../components/CourseDetailBox/CourseDetailBox";
 
 const CourseInfo = () => {
   return (
@@ -29,6 +31,94 @@ const CourseInfo = () => {
           },
         ]}
       />
+
+      <section className="course-info">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <a href="#" className="course-info__link">
+                آموزش برنامه نویسی فرانت اند
+              </a>
+              <h1 className="course-info__title">
+                آموزش 20 کتابخانه جاوااسکریپت برای بازار کار
+              </h1>
+              <p className="course-info__text">
+                امروزه کتابخانه‌ها کد نویسی را خیلی آسان و لذت بخش تر کرده اند.
+                به قدری که حتی امروزه هیچ شرکت برنامه نویسی پروژه های خود را با
+                Vanilla Js پیاده سازی نمی کند و همیشه از کتابخانه ها و فریمورک
+                های موجود استفاده می کند. پس شما هم اگه میخواید یک برنامه نویس
+                عالی فرانت اند باشید، باید کتابخانه های کاربردی که در بازار کار
+                استفاده می شوند را به خوبی بلد باشید
+              </p>
+              <div className="course-info__social-media">
+                <a href="#" className="course-info__social-media-item">
+                  <i className="fab fa-telegram-plane course-info__icon"></i>
+                </a>
+                <a href="#" className="course-info__social-media-item">
+                  <i className="fab fa-twitter course-info__icon"></i>
+                </a>
+                <a href="#" className="course-info__social-media-item">
+                  <i className="fab fa-facebook-f course-info__icon"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <video
+                src=""
+                poster={poster}
+                className="course-info__video"
+                controls
+              ></video>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <main className="main">
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <div className="course">
+                <div className="course-boxes">
+                  <div className="row">
+                    <CourseDetailBox
+                      icon="graduation-cap"
+                      title="وضعیت دوره"
+                      text="به اتمام رسیده"
+                    />
+                    <CourseDetailBox
+                      icon="clock"
+                      title="مدت زمان دوره:"
+                      text=" 19 ساعت"
+                    />
+                    <CourseDetailBox
+                      icon="calendar-alt"
+                      title=" آخرین بروزرسانی:"
+                      text=" 1401/03/02"
+                    />
+                    <CourseDetailBox
+                      icon="user-alt"
+                      title="روش پشتیبانی"
+                      text=" آنلاین"
+                    />
+                    <CourseDetailBox
+                      icon="info-circle"
+                      title="پیش نیاز:"
+                      text=" HTML CSS"
+                    />
+                    <CourseDetailBox
+                      icon="play"
+                      title="نوع مشاهده:"
+                      text="ضبط شده / آنلاین"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
       <Footer />
     </>
   );
